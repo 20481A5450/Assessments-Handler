f052525a41c02a6fcb38efde485b72bc7efa7082
Zohaib Shaik <20481A5450@gecgudlavallerumic.in>
2024-05-11 11:07:32 +0530

    chore: Include 'myapp' in INSTALLED_APPS list in settings.py

--- BackendDeveloper_Babynama/blogapp/blogapp/settings.py
+++ BackendDeveloper_Babynama/blogapp/blogapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp', # Add this line to include the app in the project
 ]
 
 MIDDLEWARE = [
